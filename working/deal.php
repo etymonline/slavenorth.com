3674e76fbdcc7dcb7c9d53371d312fa04284026c
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-12-13 12:42:04 -0500

    "/root/harperfiles/"

---
+++ working/deal.php
@@ -0,0 +1,32 @@
+<?php
+require_once("../init.php");
+
+$title = 'Sponsor A Word';
+include("page_start.php");
+?>
+
+
+<div style="text-align: center;">
+
+<table style="margin: 0 auto;">
+ <tr valign="top">
+  <td><img src="carly.jpg" width="50" height="163" alt="Etta" title="Etta" /></td>
+  <td>
+   <h1 style="text-align: center; color: #006666;">SPONSOR A WORD!</h1>
+   <p style="text-align: center; font-size: 18pt;">Here's the Deal</p>
+  </td>
+  <td><img src="kat.jpg" width="50" height="163" alt="Molly G." title="Molly G." /></td>
+ </tr>
+</table>
+
+<p>Hi, we're Etta and Molly G., and we're your tour guides. Do you enjoy the Online Etymology Dictionary? Do you love words? Now you can sponsor the word of your choice, in the Online Etymology Dictionary, for six months, for $10. You pay us by Paypal, and then we add you as a sponsor and put your message of 100 words or less and picture on our sponsor page! (You can sponsor anonymously if you're in the Witness Protection Program or somethin'). Your sponsorship helps offset the cost of hosting and maintaining this resource.</p>
+<p><i>Etta sez: "It's just like majik, except it costs ten bucks!"</i></p>
+<p>The picture will have to be a small one, or one that we can shrink. If you want to use a bigger picture, we can arrange that ... for a bigger donation (heh-heh). Pictures and content are subject to our approval, so play nice!</p>
+<p><a href="https://www.paypal.com/"><b>Just click here</b></a></p>
+<p>Use your Paypal account and send $10 U.S. to <b>byronic106@yahoo.com</b>. On the Paypal form, under the "send money" field, there's an option to send an e-mail, too. Write the word you want to sponsor as the "subject" of the e-mail. Then write the message you want to appear as the body of the e-mail message. Got it? Good! When we get the Paypal payment, we'll add you to the sponsor list. If you'd like a picture or graphic with your sponsor listing, you'll have to send that along separately (to byronic106@yahoo.com), cuz we don't think Paypal lets you make attachments.</p>
+<p><a href="sponsors.php"><b>Meet the Sponsors!</b></a></p>
+<p><i>Molly G. sez: "Save the words! Don't let them get all confused and wander off and get lost by accident!"</i></p>
+
+</div>
+
+<?php include("page_end.php"); ?>
---
+++ working/sponsors.php
@@ -0,0 +1,8 @@
+<?php
+require_once("/var/sites/etymlamp/init.php");
+
+$title = 'Sponsor A Word';
+include("page_start.php");
+include("/root/etymeditable/sponsors.php");
+include("page_end.php");
+?>
